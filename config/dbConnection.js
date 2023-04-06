ee16a1bb4899a95279e9e10e7b3bce1650f5d79c
PadalaVinayBHushan <ppadalavinaybhushan@gmail.com>
2023-04-06 10:38:15 +0530

    po

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async() =>{
     try{
        // const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        const connect = await mongoose.connect('mongodb+srv://batch6:herovired@cluster0.aqifkg2.mongodb.net/lms');
+        const connect = await mongoose.connect('mongodb+srv://batch6:herovired@cluster0.aqifkg2.mongodb.net/lms',{ useNewUrlParser: true });
 
         console.log("database connected: ",connect.connection.host);
        // console.log(connect.connections);
